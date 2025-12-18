a75200e03b0e5b46b166d1f3b00ecc0f72033f1b
Andro <Andro_milad22@yahoo.com>
2025-12-18 20:38:51 +0200

    voting system update

--- Voting.py
+++ Voting.py
@@ -10,3 +10,30 @@ class candidate:
         return self.votes
     def get_name(self):
         return self.name
+class VotingSystem:
+    def __init__(self):
+        self.candidates = {}
+
+    def add_candidate(self, candidate_name):
+        if candidate_name not in self.candidates:
+            self.candidates[candidate_name] = candidate(candidate_name)
+            print(f'Candidate "{candidate_name}" added.')
+        else:
+            print(f'Candidate "{candidate_name}" is already added before.')
+
+    def vote(self, candidate_name):
+        if candidate_name in self.candidates:
+            self.candidates[candidate_name].add_vote()
+            print(f'Vote add for "{candidate_name}".')
+        else:
+            print(f'Candidate "{candidate_name}" not.')
+
+    def get_results(self):
+        max =0
+        for cand in self.candidates.values():
+            if cand.get_votes()>max:
+                max=cand.get_votes()
+        for cand in self.candidates.values():
+            if cand.get_votes()==max:
+                winner=cand.get_name()
+        print( f'The winner is "{winner}" with {max} votes.')
